cb9cbd76988c628e1de95431fe86de8a5fc33481
Prakhar Mathur <prakharmathur043@gmail.com>
2019-01-27 14:37:39 +0530

    git commit -am 'Completed the module'

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -10,6 +11,24 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name): base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+           if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+            var temp = (int)Math.Ceiling(Students.Count * .2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if (grades[temp - 1] <= averageGrade)
+                return 'A';
+            else if (grades[temp * 2 - 1] <= averageGrade)
+                return 'B';
+            else if (grades[temp * 3 - 1] <= averageGrade)
+                return 'C';
+            else if (grades[temp * 4 - 1] <= averageGrade)
+                return 'D';
+            else return 'F';
         }
     }
 }
